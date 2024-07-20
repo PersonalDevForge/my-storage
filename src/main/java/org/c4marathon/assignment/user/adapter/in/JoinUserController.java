a471e5560088567bf02d64cd2448d7c6c80127b9
fing9 <djajsl54@naver.com>
2024-07-20 15:28:51 +0900

    [Feat]: GetUserProfile 기능 구현

--- src/main/java/org/c4marathon/assignment/user/adapter/in/JoinUserController.java
+++ src/main/java/org/c4marathon/assignment/user/adapter/in/JoinUserController.java
@@ -1,26 +1,26 @@
 package org.c4marathon.assignment.user.adapter.in;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.global.response.ApiResponse;
 import org.c4marathon.assignment.global.response.enums.ResultCode;
 import org.c4marathon.assignment.user.adapter.in.dto.request.JoinUserRequestDto;
 import org.c4marathon.assignment.user.application.port.in.JoinUserUseCase;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class JoinUserController {
 
     private final JoinUserUseCase joinUserUseCase;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> joinUser(@Validated @RequestBody JoinUserRequestDto joinUserRequestDto) {
+    public ResponseEntity<ApiResponse<Void>> joinUser(@Valid @RequestBody JoinUserRequestDto joinUserRequestDto) {
         try {
             joinUserUseCase.joinUser(joinUserRequestDto.getName(), joinUserRequestDto.getEmail(), joinUserRequestDto.getPassword());
         } catch (IllegalAccessException e) {
